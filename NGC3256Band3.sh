52b099c72c954c4cfe852d45236d2b121d4ac0dc
Jared Crossley <JaredHCrossley@gmail.com>
2013-01-16 11:38:49 -0500

    Updated path to lustre area.

--- NGC3256Band3.sh
+++ NGC3256Band3.sh
@@ -3,4 +3,4 @@
 calibrationURL='http://casaguides.nrao.edu/index.php?title=NGC3256_Band3_Calibration_for_CASA_4.0'
 imagingURL='http://casaguides.nrao.edu/index.php?title=NGC3256_Band3_Imaging_for_CASA_4.0'
 dataURL='https://almascience.nrao.edu/almadata/sciver/NGC3256/NGC3256_Band3_UnCalibratedMSandTablesForReduction.tgz'
-dataPath='/export/lustre/SV/NGC3256/NGC3256_Band3_UnCalibratedMSandTablesForReduction.tgz'
+dataPath='/lustre/naasc/SV/NGC3256/NGC3256_Band3_UnCalibratedMSandTablesForReduction.tgz'
