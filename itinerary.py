046706cdb81aa7777c6733c8445c26e1868ee8af
nbrunett <nate.brunetti@gmail.com>
2015-06-06 19:37:27 -0400

    adding for use in top_level.py

---
+++ itinerary.py
@@ -0,0 +1,36 @@
+#edit the fields below and save for the benchmarking itinerary you want
+
+#information needed:
+#  -host names
+#  -data sets
+#  -number of iterations of each data set
+#  -execution steps for each data set
+#  -sources of scripts for each data set
+#  -working directory on host
+
+
+
+itinerary = {'hosts': {'cvpost045': {'dataSets': ['x2012_1_00912_S_43', \
+                                                  'NGC3256Band3_43'], \
+                                     'nIters': [2, 2], \
+                                     'steps': ['both', 'im'], \
+                                     'scriptSources': ['disk', 'web'], \
+                                     'workDir': '/lustre/naasc/nbrunett/' + \
+                                                'bench_code_devel/testing/' + \
+                                                'test_remote_subprocess'}, \
+                       'gauss': {'dataSets': ['x2012_1_00912_S_43', \
+                                              'NGC3256Band3_43'], \
+                                 'nIters': [2, 2], \
+                                 'steps': ['both', 'im'], \
+                                 'scriptSources': ['disk', 'web'], \
+                                 'workDir': '/lustre/naasc/nbrunett/' + \
+                                            'bench_code_devel/testing/' + \
+                                            'test_remote_subprocess'}, \
+                       'technomage': {'dataSets': ['x2012_1_00912_S_43', \
+                                                   'NGC3256Band3_43'], \
+                                      'nIters': [2, 2], \
+                                      'steps': ['both', 'im'], \
+                                      'scriptSources': ['disk', 'web'],\
+                                      'workDir': '/lustre/naasc/nbrunett/' + \
+                                                 'bench_code_devel/testing/' + \
+                                                 'test_remote_subprocess'}}}
